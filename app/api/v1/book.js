99b72af1040d042788acbb43de82de998eeb9875
xian <372748785@qq.com>
2020-09-15 22:07:27 +0800

    解决冲突

--- app/api/v1/book.js
+++ app/api/v1/book.js
@@ -6,11 +6,7 @@ const router = new Router({
 
 router.get('/:id', async(ctx, next) => {
   const v = await new PositiveIdValidator().validate(ctx)
-<<<<<<< HEAD
   throw new global.errs.ParameterException()
-=======
-  throw new global.errs.ParameterException({code:10002})
->>>>>>> bug-100
 })
 
 
